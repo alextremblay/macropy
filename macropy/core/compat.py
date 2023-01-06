ee538b4d8274e9bc0636f35b22540069b4966567
Roman Zayrullin <krosenmann@gmail.com>
2023-01-06 15:04:16 +0300

    Add 3.9 compatibility option

--- macropy/core/compat.py
+++ macropy/core/compat.py
@@ -8,6 +8,7 @@ PY34 = sys.version_info >= (3, 4)
 PY35 = sys.version_info >= (3, 5)
 PY36 = sys.version_info >= (3, 6)
 PY38 = sys.version_info >= (3, 8)
+PY39 = sys.version_info >= (3,9)
 
 CPY = sys.implementation.name == 'cpython'
 PYPY = sys.implementation.name == 'pypy'
